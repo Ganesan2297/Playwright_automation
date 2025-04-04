388030d0e3ab2ebc0e15a3487a1d1bd24725ebea
Ganesan2297 <ganesh71997@gmail.com>
2025-04-04 16:27:56 +0530

    Happytest

---
+++ tests/Negative_test.spec.js
@@ -0,0 +1,48 @@
+const {test, expect} = require ('@playwright/test')
+import { text } from 'stream/consumers';
+import Config4 from './Login/Config4.json';
+
+
+test.describe('❌ Negative Login Test Cases - GreenLight Grocery', () => {
+
+         test('Invalid Username', async ({ page }) => {
+      await page.goto(Config4.baseUrl);
+      await page.getByRole('button', { name: 'Sign In' }).click();
+      await page.getByRole('textbox', { name: 'name@host.com' }).fill(Config4.invalidCredentials.wrongUsername.username);
+      await page.waitForTimeout(3000);
+      await page.getByRole('textbox', { name: 'Password' }).fill(Config4.invalidCredentials.wrongUsername.password);
+      await page.getByRole('button', { name: 'submit' }).click();
+      await page.waitForTimeout(3000);
+      
+      const error = page.locator('text=Invalid Username and Password')
+      console.log('❌ Login failed with invalid username');
+    });
+  
+      test('Invalid Password', async ({ page }) => {
+      await page.goto(Config4.baseUrl);
+      await page.getByRole('button', { name: 'Sign In' }).click();
+      await page.getByRole('textbox', { name: 'name@host.com' }).fill(Config4.invalidCredentials.wrongPassword.username);
+      page.waitForTimeout(2000);
+      await page.getByRole('textbox', { name: 'Password' }).fill(Config4.invalidCredentials.wrongPassword.password);
+      await page.getByRole('button', { name: 'submit' }).click();
+      await page.waitForTimeout(3000);
+  
+      //const error = page.locator('text=Invalid Username and Password')  
+      console.log('❌ Login failed with invalid password');
+    }); 
+  
+      test('Empty Credentials', async ({ page }) => {
+      await page.goto(Config4.baseUrl);
+      await page.getByRole('button', { name: 'Sign In' }).click();
+      await page.getByRole('button', { name: 'submit' }).click();
+      await page.waitForTimeout(2000);
+      
+  
+      const error = page.locator('text=Please fill this field');
+      console.log('❌ Login failed with empty credentials');
+    }); 
+  
+  });
+
+
+
--- tests/myfirst_test.spec.js
+++ tests/myfirst_test.spec.js
@@ -1,32 +1,99 @@
 const { test, expect } = require('@playwright/test');
-const { authenticator } = require('otplib');
+//const { authenticator } = require('otplib');
 const config = require('./Login/Config.json'); // Import JSON configuration
+test.describe.configure({ retries: 1 });
 
-test('Login Test with OTP', async ({ page }) => {
+test('Login Test with Happy Path', async ({ page }) => {
     await page.goto(config.baseUrl);
     await expect(page).toHaveTitle('GreenLight Grocery');
 
     // Click Sign In
+    await expect(page.getByRole('button', { name: 'Sign in' })).toBeEnabled();
+    await page.waitForTimeout(2000);
     await page.getByRole('button', { name: 'Sign In' }).click();
 
+
     // Enter Login Details
+    await expect(page.getByRole('textbox', { name: 'name@host.com' })).toBeVisible();
     await page.getByRole('textbox', { name: 'name@host.com' }).fill(config.username);
+
+
+    await expect(page.getByRole('textbox', { name: 'Password' })).toBeVisible()
     await page.getByRole('textbox', { name: 'Password' }).fill(config.password);
-    await page.waitForTimeout(5000);
+    await page.waitForTimeout(3000);
+
+    await expect(page.getByRole('button', { name: 'submit' })).toBeVisible();
     await page.getByRole('button', { name: 'submit' }).click();
-    
 
-    // Generate OTP
-   authenticator.options = { step: 30, window: 1 }; // Adjust OTP validity window
-    const otp = authenticator.generate(config.totpSecret);
-    console.log(`Generated OTP: ${otp}`);
 
-    // Enter OTP
-    await page.getByRole('textbox', { name: 'authentication code' }).fill(otp);
-    await page.getByRole('button', { name: 'Sign in' }).click(); 
-
+    const otpInput = page.getByRole('textbox', { name: 'authentication code' });
+    await expect(otpInput).toBeVisible({ timeout: 10000 });
+    console.log("🔹 Enter the OTP manually in the browser. Playwright will continue automatically."); // Wait up to 10s for the field to appear
+    await page.getByRole('button', { name: 'Sign in' }).click();
     console.log('✅ Login successful with OTP!');
 
+    await page.waitForTimeout(5000);
+    await page.locator('header').getByRole('button').click();
+
+    //Navigating to create a new Order
+    await expect(page.getByRole('link', { name: 'New Order' })).toBeVisible();
+    await page.waitForTimeout(1000);
+    await page.getByRole('link', { name: 'New Order' }).click();
+    await page.waitForTimeout(1000);
+    
+    //Selecting one of the Dsiplayed Restaurant
+    await page.getByText('Americano, Lincoln ParkChicago, IL').click();
+    await expect(page.getByRole('option', { name: 'The Taco Stop Fort Collins, CT' })).toBeVisible();
+    await page.getByRole('option', { name: 'The Taco Stop Fort Collins, CT' }).click();
+
+    await page.getByRole('tab', { name: 'All Products' }).click();
+
+    await expect(page.getByRole('textbox', { name: 'Search All Products by name...' })).toBeVisible();
+    await page.getByRole('textbox', { name: 'Search All Products by name...' }).click();
+    
+    await expect(page.getByRole('textbox', { name: 'Search All Products by name...' })).toBeEditable();
+    await page.getByRole('textbox', { name: 'Search All Products by name...' }).fill('chilli');
+    
+    await expect(page.getByRole('button', { name: 'Search' })).toBeEnabled();
+    await page.getByRole('button', { name: 'Search' }).click();
+    
+    await page.waitForSelector('.flex.flex-col.items-start.py-4.px-8.w-full.overflow-auto', { timeout: 10000 });
+
+    //Selecting the required item from the list
+    
+    await page.locator('.bg-zinc-800\\/90 > button').first().click();
+    await page.getByRole('textbox').nth(1).click();
+    await page.getByRole('textbox').nth(1).fill('10');
+    
+
+    await page.waitForSelector('button:has-text("Checkout")', { state: 'visible', timeout: 5000 });
+    await page.getByRole('button', { name: 'Checkout' }).click();
+
+    
+    const sendEmailButton = page.getByRole('button', { name: 'Send Email Total: $' });
+    await sendEmailButton.scrollIntoViewIfNeeded();
+    await sendEmailButton.click();
+    
+   
+
+   // Assert that the "Send Email" button is visible and enabled, then click it
+   
+    await expect(page.getByRole('button', { name: 'Send Email' })).toBeVisible();
+    await expect(page.getByRole('button', { name: 'Send Email' })).toBeEnabled();
+    await page.getByRole('button', { name: 'Send Email' }).click();
+    console.log("Email Sent Successfully");
+    
+
+// Assert that the "Return Ordering" button is visible and enabled, then click it
+    await expect(page.getByRole('button', { name: 'Return Ordering' })).toBeEnabled();
+    await page.getByRole('button', { name: 'Return Ordering' }).click();
+    
 
 });
+
+
+
+
+
+
 
